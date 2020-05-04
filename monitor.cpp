c9403fa0588cf9b6687f8048a0183ecd0d26dadb
petr <petr.buben@gmail.com>
2020-05-04 14:21:46 +0100

    v2 locking, remove Makefile

--- monitor.cpp
+++ monitor.cpp
@@ -10,16 +10,17 @@ std::mutex m;
 int monitor(char ** result, int pi){
     *result = new char[256];
     int in;
+    m.lock();
     
      std::cout<<"device enter 1 digit number, thread " << pi <<std::endl;
      
      if (!(std::cin))  std::cin.clear(); // cin fix
      std::cin>>in;
      
-     if (m.try_lock()){
+    
      g_soucet += in;  //critical code
      m.unlock();
-     }
+    
      
      snprintf(*result, 256, "Thread id:%d  data in:%d  soucet %d  function number %d", std::this_thread::get_id(), in, g_soucet, pi);
      
