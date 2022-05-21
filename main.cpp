3352eecdf7e2ebad16d8f19874dcfcdf42f64285
Petr Buben <petr.buben@gmail.com>
2022-05-21 16:28:26 +0200

    2022 modified,add data read count, some code edits

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 //threads demo, one function serving threads
-//with future promise, conditional variable/lock and atomic variable
+//with future promise, conditional variable/lock and atomic variable functionality demonstration
 //by Petr Buben
+//slightly modified, 2022
 
 #include <iostream>
 #include <thread>
@@ -10,71 +11,71 @@
 #include <mutex>
 #include <condition_variable>
 
-using namespace std;
+//using namespace std;
 
 typedef struct Device{
 		int c=0;
 		int i=0;
 	} S;
     
-atomic<int> aisum;
+std::atomic<int> aisum;
 int copyPromise = 0;
-condition_variable cv;
-mutex m;
+std::condition_variable cv;
+std::mutex m;
 
-void ft(Device& dev, promise<int>& pr, future<int>& fu) {
-    this_thread::sleep_for(chrono::seconds(dev.i));
+void ft(Device& dev, std::promise<int>& pr, std::future<int>& fu) {
+	std::this_thread::sleep_for(std::chrono::seconds(dev.i));
 
 	if(dev.i==0){
-		cout<<"thread id "<<dev.c<<" as supplier of future promise\n";
-		cout<<"promise set as "<<dev.i+8<<endl;
-        cout<<"copyPromise "<<copyPromise<<endl;
+		std::cout<<"thread id "<<dev.c<<" as supplier of future promise\n";
+		std::cout<<"promise set as "<<dev.i+8<<std::endl;
+		std::cout<<"copyPromise "<<copyPromise<<std::endl;
         pr.set_value(dev.i+8);//fut get is waiting for this, above first
         
         //cv
-        std::lock_guard<mutex> lg(m);
+        std::lock_guard<std::mutex> lg(m);
         copyPromise = dev.i + 8;
         cv.notify_one(); //notifies cv.wait thread
 	}
 	else{
 		if(dev.i==1){
             int f=fu.get(); //first wait, then cout
-        cout<<"\nthread id "<<dev.c<<" as receiver of future promise\n";
-		cout<<"receiver thread id "<<dev.c<<" received future promise as "<< f<<endl<<endl;   
+	    std::cout<<"\nthread id "<<dev.c<<" as receiver of future promise\n";
+	    std::cout<<"receiver thread id "<<dev.c<<" received future promise as "<< f<<std::endl<<std::endl;   
               
         //cv
-        unique_lock<mutex> ul(m); //must be unique
+	    std::unique_lock<std::mutex> ul(m); //must be unique
         cv.wait(ul,[] {return (copyPromise !=0)? true : false;});
-        cout<<"after cv.wait, copyPromise now is " << copyPromise<<endl<<endl;
-        cout<<"data sum "<<aisum<<endl;
+	std::cout<<"after cv.wait, copyPromise now is " << copyPromise<<std::endl<<std::endl;
+	std::cout<<"data sum "<<aisum<<std::endl;
         }
    
     aisum+=dev.i;
-    cout<<"receiver thread id "<<dev.c <<" data read "<<dev.i<<", sum is "<< aisum<<endl; 
+    std::cout<<"receiver thread id "<<dev.c <<" data read "<<dev.i<<", data sum is "<< aisum<<std::endl; 
 	}
 } 
 
 int main() {
-	promise<int> prom;
-	future<int> fut;
+	std::promise<int> prom;
+	std::future<int> fut;
 	fut=prom.get_future();
     
-    int hw = thread::hardware_concurrency();
-    cout<<"number of cores available "<<hw<<endl<<endl;
+    int hw = std::thread::hardware_concurrency();
+    std::cout<<"number of cores available "<<hw<<std::endl<<std::endl;
 	
-	vector<Device> vdev;
-	for(int d=0;d<hw;d++){
+    	std::vector<Device> vdev;
+	for(int d=0; d<hw; d++){
 		vdev.push_back(Device {d,d});
 	}
 	
-	vector <thread > vt;
-	for(int i=0;i<hw;i++)
-		vt.push_back (thread (ft,ref(vdev[i]), ref(prom), ref(fut)));
+	std::vector <std::thread > vt;
+	for(int i=0; i<hw; i++)
+		vt.push_back (std::thread (ft,std::ref(vdev[i]), std::ref(prom), std::ref(fut)));
 	
-	for(thread &t:vt) 
-	t. join() ;
+	for(std::thread &t:vt) 
+	t.join() ;
     
-    cout<<"\nmain thread total sum "<<aisum<<endl<<endl;
-
+	std::cout<<"\nmain thread total data sum "<<aisum<<std::endl;
+	std::cout<<"number of data reads: " <<hw<<std::endl;
 	return 0;
 }
